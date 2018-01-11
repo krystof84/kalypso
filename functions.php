53db07ec667a025dcdc877a50b7dc7f1164e3f2b
Krzysztof <krisczajka@gmail.com>
2018-01-11 20:59:56 +0100

    Add setup theme function. Register and display nav menus.

--- functions.php
+++ functions.php
@@ -17,49 +17,77 @@ if ( ! class_exists( 'Timber' ) ) {
 
 Timber::$dirname = array('templates', 'views');
 
-class StarterSite extends TimberSite {
 
-	function __construct() {
-		add_theme_support( 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'menus' );
-		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
-		parent::__construct();
-	}
+// Define Google Maps API key
+define('GMAP_KEY', 'xxxxxx');
 
-	function register_post_types() {
-		//this is where you can register custom post types
-	}
+/**
+ * Add common modules to the view context
+ *
+ * @param array $context
+ */
+function kalypso_add_to_context( $context ) {
+    // Add menus to context
+    $context['menu'] = [
+        'primary' => new \Timber\Menu( 'primary' ),
+        'footer' => new \Timber\Menu( 'footer' ),
+    ];
 
-	function register_taxonomies() {
-		//this is where you can register custom taxonomies
-	}
-
-	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new TimberMenu();
-		$context['site'] = $this;
-		return $context;
-	}
-
-	function myfoo( $text ) {
-		$text .= ' bar!';
-		return $text;
-	}
-
-	function add_to_twig( $twig ) {
-		/* this is where you can add your own functions to twig */
-		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
-		return $twig;
-	}
-
+    return $context;
 }
+add_filter( 'timber/context', 'kalypso_add_to_context' );
 
-new StarterSite();
+
+if ( ! function_exists( 'bstone_fonts_url' ) ) :
+    /**
+     * Register Google fonts.
+     */
+    function kalypso_fonts_url() {
+        $fonts_url = '';
+        $fonts     = array(
+            'Source+Sans+Pro:300,400,600,700',
+        );
+        $subsets   = 'latin,latin-ext';
+
+        if ( $fonts ) {
+            $fonts_url = add_query_arg( array(
+                'family' => implode( '|', $fonts ),
+                'subset' => $subsets,
+            ), 'https://fonts.googleapis.com/css?family=' );
+        }
+
+        return $fonts_url;
+    }
+endif;
+
+/**
+ * Setup theme
+ */
+if ( ! function_exists( 'kalypso_setup' ) ) :
+    function kalypso_setup() {
+
+        load_theme_textdomain( 'kalypso' );
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support( 'title-tag' );
+
+        add_theme_support( 'post-thumbnails' );
+
+        register_nav_menus( array(
+            'primary' => __( 'Menu Główne', 'kalypso' ),
+            'footer' => __( 'Menu w stopce', 'kalypso' ),
+        ) );
+
+        add_theme_support( 'html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ) );
+
+        add_editor_style( array( 'build/styles/editor-style.css', kalypso_fonts_url() ) );
+    }
+endif; // kalypso_setup
+add_action( 'after_setup_theme', 'kalypso_setup' );
