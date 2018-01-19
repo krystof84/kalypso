13cb82a685178bf5615e61eaafba0bae153cb877
Krzysztof <krisczajka@gmail.com>
2018-01-19 07:20:42 +0100

    Cleanup front-page.php

--- front-page.php
+++ front-page.php
@@ -7,15 +7,12 @@ $context = Timber::get_context();
 $post = new TimberPost();
 
 $context['post'] = $post;
-$context['slide1url'] = get_template_directory_uri() . '/src/img/demo/slider-1.png';
-$context['slide2url'] = get_template_directory_uri() . '/src/img/demo/slider-2.png';
-$context['slide3url'] = get_template_directory_uri() . '/src/img/demo/slider-3.png';
 
 $slidesArgs = array(
     'post_type' => 'slide',
-    'order_by' => 'ASC',
+    'order' => 'ASC',
+    'order_by' => 'menu_order',
     'posts_per_page' => -1,
-
 );
 
 $context['slides'] = Timber::get_posts( $slidesArgs );
